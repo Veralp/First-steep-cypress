867eff76f89893ca4efd242b4f5dc3ddcbfbd4a4
Vera Lúcia <veradcj.fl@hotmail.com>
2024-08-14 09:42:41 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,31 @@
+import userData from '../fixtures/userData.json';
+
+
+describe('Orange HRM test', () => {
+
+  const selectorList = {
+    usernameField:"[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid:".orangehrm-dashboard-grid",
+    wrongCredentialAlert:"[role='alert']"
+  
+  }
+  it('Login success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+      })
+      it('Login-Fail', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get(selectorList.usernameField).type(userData.userFail.username)
+        cy.get(selectorList.passwordField).type(userData.userFail.password)
+        cy.get(selectorList.loginButton).click()
+        cy.get(selectorList.wrongCredentialAlert)
+        
+    })
+})
