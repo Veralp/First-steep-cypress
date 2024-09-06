32564ad231eb0b9bfb4c692b6cc037cf3b04a449
Vera Lúcia <veradcj.fl@hotmail.com>
2024-09-05 21:16:54 -0300

    User added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -9,18 +9,43 @@ describe('Orange HRM test', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid:".orangehrm-dashboard-grid",
-    wrongCredentialAlert:"[role='alert']"
-  
+    wrongCredentialAlert:"[role='alert']",
+    myInfoButton:'[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField:"[name='lastName']",
+    //midleNamefield: "[name='middleName']",
+    genericField:".oxd-input--active",
+    dataField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton:".--close",
+    SubmitButton:"[type = 'Submit']"
   }
-  it('Login success', () => {
+  it.only('User Info Update success', () => {
     cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
-      })
-      it('Login-Fail', () => {
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).type('FirstNameTest')
+    //cy.get(selectorList.midleNamefield).type('MidleNameTest')
+    cy.get(selectorList.lastNameField).type('LastNameTest')
+    cy.get(selectorList.genericField).eq(5).clear().type('Employee')
+    cy.get(selectorList.genericField).eq(6).clear().type('OtherIdtest')
+    cy.get(selectorList.genericField).eq(8).clear().type('DriveLicenseTest')
+    //cy.get(selectorList.genericField).eq(8).clear().type('ssnNumberTest')
+    cy.get(selectorList.genericField).eq(7).clear().type('2025-03-10')
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.SubmitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully update')
+
+    
+
+
+
+
+  })
+  it('Login-Fail', () => {
         cy.visit('/auth/login')
         cy.get(selectorList.usernameField).type(userData.userFail.username)
         cy.get(selectorList.passwordField).type(userData.userFail.password)
