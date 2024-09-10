c0f4244c656efb989f89711c3dfb018e006d7942
Vera Lúcia <veradcj.fl@hotmail.com>
2024-09-10 11:21:37 -0300

    fixing the code

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -21,24 +21,24 @@ describe('Orange HRM test', () => {
   }
   it.only('User Info Update success', () => {
     cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).clear().type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).clear().type(userData.userSuccess.password)
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
     cy.get(selectorList.myInfoButton).click()
-    cy.get(selectorList.firstNameField).type('FirstNameTest')
+    cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
     //cy.get(selectorList.midleNamefield).type('MidleNameTest')
-    cy.get(selectorList.lastNameField).type('LastNameTest')
+    cy.get(selectorList.lastNameField).clear().type('LastNameTest')
     cy.get(selectorList.genericField).eq(5).clear().type('Employee')
     cy.get(selectorList.genericField).eq(6).clear().type('OtherIdtest')
     cy.get(selectorList.genericField).eq(8).clear().type('DriveLicenseTest')
     //cy.get(selectorList.genericField).eq(8).clear().type('ssnNumberTest')
     cy.get(selectorList.genericField).eq(7).clear().type('2025-03-10')
-    cy.get(selectorList.dateCloseButton).click()
+    //cy.get(selectorList.dateCloseButton).click()
     cy.get(selectorList.SubmitButton).eq(0).click()
-    //cy.get('body').should('contain', 'Successfully Update')
-    cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button')
+    cy.get('.oxd-toast').should('contain', 'Successfully Update')
+    //cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .')
     
 
 
